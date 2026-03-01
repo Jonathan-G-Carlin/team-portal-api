4c8ec451ad3b245178ac12b4e04dad8cc50b7f68
Jonathan Carlin <Jonathan.G.Carlin@outlook.com>
2026-03-01 01:35:36 -0500

    configure TeamPortalDbConnection in appsettings and wire up in Program.cs

--- TeamPortal.Api/Program.cs
+++ TeamPortal.Api/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TeamPortalDbConnection"));
 });
 
 
