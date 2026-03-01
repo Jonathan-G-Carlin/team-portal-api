0f29babac89ec3c0dba0117edd88e681c8c2239d
Jonathan Carlin <Jonathan.G.Carlin@outlook.com>
2026-03-01 01:09:00 -0500

    register AppDbContext in DI and configure SQL Server

--- TeamPortal.Api/Program.cs
+++ TeamPortal.Api/Program.cs
@@ -1,4 +1,14 @@
+using TeamPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
