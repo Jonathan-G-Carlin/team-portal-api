1bc6e81654161c5e0e17df1b01ec502799e749ea
Jonathan Carlin <Jonathan.G.Carlin@outlook.com>
2026-03-01 22:20:37 -0500

    chore: configure controllers, OpenAPI, and Scalar UI in Program.cs with middleware ordering comments

--- TeamPortal.Api/Program.cs
+++ TeamPortal.Api/Program.cs
@@ -1,17 +1,34 @@
 using TeamPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using TeamPortal.Api.Services;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// DbContext registration
 builder.Services.AddDbContext<AppDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("TeamPortalDbConnection"));
 });
 
+// Controller registration
+builder.Services.AddControllers();
+
+// Service registration
 builder.Services.AddScoped<UserService>();
+
+// OpenAPI registration
+builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Authenication and Authorization middlewares would go here (Before Map Controller)
+
+// Map controllers (After all Routing and Auth middlewares)
+app.MapControllers();
+
+app.MapOpenApi(); // Add MappOpenApi(), Then  MapScalarApiReference()  because it adds    /openapi endpoint   which is used by the latter);
+app.MapScalarApiReference();
+
 
 app.Run();
